e7cf5e8dd887024b920c7a109b3c0c9083b6a3df
Loren Aguey <loren@blackcatdev.io>
2022-12-15 10:25:47 -0500

    add log entry to schema

--- src/util.entity.ts
+++ src/util.entity.ts
@@ -2,11 +2,13 @@ import mongoose from 'mongoose';
 
 export class UtilDTO {
   data: any;
+  log: any;
 }
 
 export const UtilSchema = new mongoose.Schema(
   {
     data: Object,
+    log: Object,
   },
   { timestamps: true },
 );
