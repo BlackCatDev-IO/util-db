03d35ef8d04eb5696588ed923589792ce95c7918
Loren Aguey <loren@blackcatdev.io>
2023-04-01 16:34:43 -0400

    Update DebugLogDTO

--- src/debug.log.entity.ts
+++ src/debug.log.entity.ts
@@ -2,11 +2,17 @@ import mongoose from 'mongoose';
 
 export class DebugLogDTO {
   log: string;
+  device: string;
+  os: string;
+  appVersion: string;
 }
 
 export const DebugLogSchema = new mongoose.Schema(
   {
     log: String,
+    device: String,
+    os: String,
+    appVersion: String,
   },
   { timestamps: true },
 );
